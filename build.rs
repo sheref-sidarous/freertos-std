c297be99b1e5c8450764e888e401acf874f00dd2
Sheref Sidarous <sherefyounan@gmail.com>
2023-07-24 08:51:57 -0700

    building shim.c as part of the build process, but need to give too many FreeRTOS relates pathes

--- build.rs
+++ build.rs
@@ -1,56 +1,24 @@
 use std::env;
+use cc;
 
 fn main() {
+    println!("cargo:rustc-cfg=feature=\"restricted-std\"");
     println!("cargo:rerun-if-changed=build.rs");
-    let target = env::var("TARGET").expect("TARGET was not set");
-    if target.contains("freebsd") {
-        if env::var("RUST_STD_FREEBSD_12_ABI").is_ok() {
-            println!("cargo:rustc-cfg=freebsd12");
-        } else if env::var("RUST_STD_FREEBSD_13_ABI").is_ok() {
-            println!("cargo:rustc-cfg=freebsd12");
-            println!("cargo:rustc-cfg=freebsd13");
-        }
-    } else if target.contains("linux")
-        || target.contains("netbsd")
-        || target.contains("dragonfly")
-        || target.contains("openbsd")
-        || target.contains("solaris")
-        || target.contains("illumos")
-        || target.contains("apple-darwin")
-        || target.contains("apple-ios")
-        || target.contains("apple-watchos")
-        || target.contains("uwp")
-        || target.contains("windows")
-        || target.contains("fuchsia")
-        || (target.contains("sgx") && target.contains("fortanix"))
-        || target.contains("hermit")
-        || target.contains("l4re")
-        || target.contains("redox")
-        || target.contains("haiku")
-        || target.contains("vxworks")
-        || target.contains("wasm32")
-        || target.contains("wasm64")
-        || target.contains("asmjs")
-        || target.contains("espidf")
-        || target.contains("solid")
-        || target.contains("nintendo-3ds")
-        || target.contains("nto")
-    {
-        // These platforms don't have any special requirements.
-    } else {
-        // This is for Cargo's build-std support, to mark std as unstable for
-        // typically no_std platforms.
-        // This covers:
-        // - os=none ("bare metal" targets)
-        // - mipsel-sony-psp
-        // - nvptx64-nvidia-cuda
-        // - arch=avr
-        // - tvos (aarch64-apple-tvos, x86_64-apple-tvos)
-        // - uefi (x86_64-unknown-uefi, i686-unknown-uefi)
-        // - JSON targets
-        // - Any new targets that have not been explicitly added above.
-        println!("cargo:rustc-cfg=feature=\"restricted-std\"");
-    }
+    println!("cargo:rerun-if-changed=src/sys/freertos/rust_std_shim.c");
     println!("cargo:rustc-env=STD_ENV_ARCH={}", env::var("CARGO_CFG_TARGET_ARCH").unwrap());
-    println!("cargo:rustc-cfg=backtrace_in_libstd");
+    let target = env::var("TARGET").expect("TARGET was not set");
+
+    if !target.contains("none") {
+        println!("cargo:warning=\"This library is intended for none targets\"");
+    }
+
+    //let freertos_config_path = env::var("FREERTOS_CONFIG_PATH").expect("Need to know where is FREERTOS_CONFIG_PATH");
+    cc::Build::new()
+        .file("src/sys/freertos/rust_std_shim.c")
+        .include("/home/shiro/projects/rust/freertos-std-example/FreeRTOS-Kernel/include")
+        .include("/home/shiro/projects/rust/freertos-std-example/FreeRTOS-Qemu-Demo")
+        .include("/home/shiro/projects/rust/freertos-std-example/FreeRTOS-Kernel/portable/GCC/ARM_CM3")
+        .compile("freertos_shim");
+
+
 }
