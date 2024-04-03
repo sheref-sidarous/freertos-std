e5a0aa7c96694add88feaa84b6297989db6e34dc
Sheref Sidarous <sherefyounan@gmail.com>
2024-04-03 07:52:45 -0700

    removed deny(ffi_unwind_calls)

--- src/lib.rs
+++ src/lib.rs
@@ -223,8 +223,6 @@
 #![allow(unused_lifetimes)]
 #![deny(rustc::existing_doc_keyword)]
 #![deny(fuzzy_provenance_casts)]
-// Ensure that std can be linked against panic_abort despite compiled with `-C panic=unwind`
-#![deny(ffi_unwind_calls)]
 // std may use features in a platform-specific way
 #![allow(unused_features)]
 //
