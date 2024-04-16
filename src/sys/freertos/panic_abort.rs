34a8cbbdd8092201ba04264b4997a34febd521eb
Sheref Sidarous <sherefyounan@gmail.com>
2024-04-15 20:12:16 -0700

    this no_mangle is necessary for linking

--- src/sys/freertos/panic_abort.rs
+++ src/sys/freertos/panic_abort.rs
@@ -2,6 +2,7 @@ use crate::sys::freertos::freertos_api;
 use core::panic::PanicPayload;
 use core::any::Any;
 
+#[no_mangle]
 pub unsafe extern "C" fn __rust_panic_cleanup(_: *mut u8) -> *mut (dyn Any + Send + 'static) {
     unreachable!()
 }
